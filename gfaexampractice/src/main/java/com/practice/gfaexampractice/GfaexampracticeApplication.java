a4eaac1510f12437792e6c6dc1700132ba379d3c
khajnal <hajnal.krisztina03@gmail.com>
2018-11-03 12:02:05 +0100

    Project init

---
+++ gfaexampractice/src/main/java/com/practice/gfaexampractice/GfaexampracticeApplication.java
@@ -0,0 +1,12 @@
+package com.practice.gfaexampractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfaexampracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GfaexampracticeApplication.class, args);
+  }
+}
